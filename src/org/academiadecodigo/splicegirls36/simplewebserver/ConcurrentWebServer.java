8dfda4240999603fc2bb67cca8dbd88e04fc08bd
Dominor <vitor.dominor@gmail.com>
2019-10-31 15:02:24 +0000

    Added fixed thread pool executor instead of raw thread creation, so that my web server doesn't explode with too many simultaneous requests. Still need to test, probably by creating a program to simulate multiple simultaneous requests.

--- src/org/academiadecodigo/splicegirls36/simplewebserver/ConcurrentWebServer.java
+++ src/org/academiadecodigo/splicegirls36/simplewebserver/ConcurrentWebServer.java
@@ -3,10 +3,14 @@ package org.academiadecodigo.splicegirls36.simplewebserver;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class ConcurrentWebServer {
 
-    public static String CHARSET = "UTF8";
+    public final static String CHARSET = "UTF8";
+    public final static int MAX_CONNECTIONS = 9999;
 
     public static final File NOT_FOUND_HTML_FILE = new File("www/404.html");
     public static final File NOT_IMPLEMENTED_FILE = new File("www/501.html");
@@ -34,18 +38,17 @@ public class ConcurrentWebServer {
 
         Socket clientSocket = null;
         Thread dispatcherThread = null;
+        ExecutorService dispatcherExecutors = Executors.newFixedThreadPool(MAX_CONNECTIONS);
 
         try {
 
             while (serverSocket.isBound()) {
 
-                System.out.println(Thread.currentThread().getName());
                 // block waiting for a client to connect
                 System.out.println("INFO: Waiting for a client connection.");
                 clientSocket = serverSocket.accept();
 
-                dispatcherThread = new Thread(new Dispatcher(clientSocket));
-                dispatcherThread.start();
+                dispatcherExecutors.submit(new Dispatcher(clientSocket));
 
             }
         } catch(IOException e){
